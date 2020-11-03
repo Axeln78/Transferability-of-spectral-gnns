9c92789ae3085f83745d9f17bf02f08e13e3c4e9
Axel Bernard Gunnar Nilsson <axel.nilsson@epfl.ch>
2020-11-03 15:27:12 +0100

    fixed output

--- Benchmark-gnn/main_SBMs_node_classification.py
+++ Benchmark-gnn/main_SBMs_node_classification.py
@@ -349,13 +349,13 @@ def main():
     net_params['n_classes'] = torch.unique(dataset.train[0][1], dim=0).size(0)
 
     root_log_dir = out_dir + 'logs/' + MODEL_NAME + "_" + DATASET_NAME + "_GPU" + str(
-        config['gpu']['id']) + "_" + time.strftime('%Hh%Mm%Ss_on_%b_%d_%Y')
+        config['gpu']['id']) + "_" + str(params['seed']) + "_" + time.strftime('%Hh%Mm%Ss_on_%b_%d_%Y')
     root_ckpt_dir = out_dir + 'checkpoints/' + MODEL_NAME + "_" + DATASET_NAME + "_GPU" + str(
-        config['gpu']['id']) + "_" + time.strftime('%Hh%Mm%Ss_on_%b_%d_%Y')
+        config['gpu']['id']) + "_" + str(params['seed']) + "_" + time.strftime('%Hh%Mm%Ss_on_%b_%d_%Y')
     write_file_name = out_dir + 'results/result_' + MODEL_NAME + "_" + DATASET_NAME + "_GPU" + str(
-        config['gpu']['id']) + "_" + time.strftime('%Hh%Mm%Ss_on_%b_%d_%Y')
+        config['gpu']['id']) + "_" + str(params['seed']) + "_" + time.strftime('%Hh%Mm%Ss_on_%b_%d_%Y')
     write_config_file = out_dir + 'configs/config_' + MODEL_NAME + "_" + DATASET_NAME + "_GPU" + str(
-        config['gpu']['id']) + "_" + time.strftime('%Hh%Mm%Ss_on_%b_%d_%Y')
+        config['gpu']['id']) + "_" + str(params['seed']) + "_" + time.strftime('%Hh%Mm%Ss_on_%b_%d_%Y')
     dirs = root_log_dir, root_ckpt_dir, write_file_name, write_config_file
 
     if not os.path.exists(out_dir + 'results'):
