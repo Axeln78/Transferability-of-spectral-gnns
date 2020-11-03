b59b00223662792172915db6cd5dc8c6867cdd44
Axel Bernard Gunnar Nilsson <axel.nilsson@epfl.ch>
2020-11-03 10:44:53 +0100

    Updated OGB script for faster processing, remaining to see if it connects well with the GPUs

--- OGB/script_ogb.sh
+++ OGB/script_ogb.sh
@@ -4,7 +4,7 @@ FILENAME=out
 tmux new -s ogb -d
 tmux send-keys "source activate benchmark_gnn" C-m
 tmux send-keys "
-python main_dgl.py --dataset 'ogbg-molhiv' --gnn Cheb_net --filename $FILENAME &
-python main_dgl.py --dataset 'ogbg-molpcba' --gnn Cheb_net --filename $FILENAME
+python main_dgl.py --dataset 'ogbg-molhiv' --batch_size 128 --gnn Cheb_net --filename $FILENAME &
+python main_dgl.py --dataset 'ogbg-molpcba' --batch_size 128 --gnn Cheb_net --filename $FILENAME
 wait" C-m
 tmux send-keys "tmux kill-session -t benchmark" C-m
