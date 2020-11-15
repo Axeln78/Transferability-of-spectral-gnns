87ae4d745ca5cc8aa86c3f76f1a7e66b74d7a471
Axeln78 <axeln78@gmail.com>
2020-11-15 18:24:56 +0100

    Updated scripts

--- Benchmark-gnn/benchmark_gnn_script.sh
+++ Benchmark-gnn/benchmark_gnn_script.sh
@@ -8,29 +8,29 @@ seed3=35
 code=main_molecules_graph_regression.py
 dataset=ZINC
 tmux new -s benchmark -d
-tmux send-keys "source activate benchmark_gnn" C-m
+tmux send-keys "conda activate benchmark_gnn" C-m
 tmux send-keys "
 python $code --dataset $dataset --gpu_id 0 --seed $seed0 --config 'configs/molecules_graph_regression_Cheb_ZINC.json' &
-python $code --dataset $dataset --gpu_id 1 --seed $seed1 --config 'configs/molecules_graph_regression_Cheb_ZINC.json' &
-python $code --dataset $dataset --gpu_id 2 --seed $seed2 --config 'configs/molecules_graph_regression_Cheb_ZINC.json' &
-python $code --dataset $dataset --gpu_id 3 --seed $seed3 --config 'configs/molecules_graph_regression_Cheb_ZINC.json' &
+python $code --dataset $dataset --gpu_id 0 --seed $seed1 --config 'configs/molecules_graph_regression_Cheb_ZINC.json' &
+python $code --dataset $dataset --gpu_id 0 --seed $seed2 --config 'configs/molecules_graph_regression_Cheb_ZINC.json' &
+python $code --dataset $dataset --gpu_id 0 --seed $seed3 --config 'configs/molecules_graph_regression_Cheb_ZINC.json' &
 wait" C-m
 
-code=main_SBMs_node_regression.py
+code=main_SBMs_node_classification.py
 dataset=SBM_PATTERN
 tmux send-keys "
-python $code --dataset $dataset --gpu_id 0 --seed $seed0 --config 'configs/molecules_graph_regression_Cheb_PATTERN.json' &
-python $code --dataset $dataset --gpu_id 1 --seed $seed1 --config 'configs/molecules_graph_regression_Cheb_PATTERN.json' &
-python $code --dataset $dataset --gpu_id 2 --seed $seed2 --config 'configs/molecules_graph_regression_Cheb_PATTERN.json' &
-python $code --dataset $dataset --gpu_id 3 --seed $seed3 --config 'configs/molecules_graph_regression_Cheb_PATTERN.json' &
+python $code --dataset $dataset --gpu_id 0 --seed $seed0 --config 'configs/SBMs_node_clustering_Cheb_PATTERN.json' &
+python $code --dataset $dataset --gpu_id 0 --seed $seed1 --config 'configs/SBMs_node_clustering_Cheb_PATTERN.json' &
+python $code --dataset $dataset --gpu_id 0 --seed $seed2 --config 'configs/SBMs_node_clustering_Cheb_PATTERN.json' &
+python $code --dataset $dataset --gpu_id 0 --seed $seed3 --config 'configs/SBMs_node_clustering_Cheb_PATTERN.json' &
 wait" C-m
 
-code=main_SBMs_node_regression.py
+code=main_SBMs_node_classification.py
 dataset=SBM_CLUSTER
 tmux send-keys "
-python $code --dataset $dataset --gpu_id 0 --seed $seed0 --config 'configs/molecules_graph_regression_Cheb_CLUSTER.json' &
-python $code --dataset $dataset --gpu_id 1 --seed $seed1 --config 'configs/molecules_graph_regression_Cheb_CLUSTER.json' &
-python $code --dataset $dataset --gpu_id 2 --seed $seed2 --config 'configs/molecules_graph_regression_Cheb_CLUSTER.json' &
-python $code --dataset $dataset --gpu_id 3 --seed $seed3 --config 'configs/molecules_graph_regression_Cheb_CLUSTER.json' &
+python $code --dataset $dataset --gpu_id 0 --seed $seed0 --config 'configs/SBMs_node_clustering_Cheb_CLUSTER.json' &
+python $code --dataset $dataset --gpu_id 0 --seed $seed1 --config 'configs/SBMs_node_clustering_Cheb_CLUSTER.json' &
+python $code --dataset $dataset --gpu_id 0 --seed $seed2 --config 'configs/SBMs_node_clustering_Cheb_CLUSTER.json' &
+python $code --dataset $dataset --gpu_id 0 --seed $seed3 --config 'configs/SBMs_node_clustering_Cheb_CLUSTER.json' &
 wait" C-m
 tmux send-keys "tmux kill-session -t benchmark" C-m
--- OGB/script_ogb.sh
+++ OGB/script_ogb.sh
@@ -2,7 +2,7 @@
 
 FILENAME=out
 tmux new -s ogb -d
-tmux send-keys "source activate benchmark_gnn" C-m
+tmux send-keys "conda activate benchmark_gnn" C-m
 tmux send-keys "
 python main_dgl.py --dataset 'ogbg-molhiv' --batch_size 128 --gnn Cheb_net --filename $FILENAME &
 python main_dgl.py --dataset 'ogbg-molpcba' --batch_size 128 --gnn Cheb_net --filename $FILENAME
